8359fd855455549d14482bd22346fff61cf3b4bc
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-09-09 20:32:13 -0300

    new reducer to list all data

---
+++ src/components/ListItem.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Text, TouchableWithoutFeedback, View} from 'react-native';
+import {connect} from 'react-redux';
+import {Card} from './common';
+import * as actions from '../actions'
+
+class ListItem extends Component {
+    render() {
+        const {titleStyle} = styles;
+        const {id, title} = this.props.library;
+
+        return(
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectedLibrary(id)}
+            >
+                <View>
+                    <Card>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </Card>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15,
+        paddingTop: 15,
+        paddingBottom: 15,
+    }
+}
+
+export default connect(null, actions)(ListItem);
